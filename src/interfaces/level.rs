98c204ca8e563ea15fc02430d9c593f4ddb66353
Alexander D. Troelsen <LoZander@outlook.com>
2022-12-21 19:39:16 +0100

    Changed name, description and targets functions to return slices

--- src/interfaces/level.rs
+++ src/interfaces/level.rs
@@ -1,15 +1,17 @@
+use std::rc::Rc;
+
 use super::{game::Direction, player::Target};
 
 pub trait Room {
-    fn name(&self) -> String;
-    fn description(&self) -> String;
+    fn name(&self) -> &str;
+    fn description(&self) -> &str;
     fn targets(&self) -> &[Target];
-    fn adjacent_room(&self, direction: Direction) -> Option<&Self>;
+    fn adjacent_room(&self, direction: Direction) -> Option<Rc<Self>>;
 }
 
 pub trait Level<R: Room> {
     fn name(&self) -> String;
     fn description(&self) -> String;
     fn rooms(&self) -> &[R];
-    fn insert_room(self,room: impl Room);
+    fn insert_room(self,room: R);
 }
